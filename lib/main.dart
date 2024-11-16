8c40714a7a84ba8662b61772e83ce2383ea93723
januarpancaran <januar352@gmail.com>
2024-11-16 22:35:59 +0700

    Adding Navigations

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,88 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
+
+// MainPage Start
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late PageController _pageController;
+
+  final List<Widget> _pages = const [
+    MyTicketPage(),
+    HomePage(),
+    AccountPage(),
+  ];
+
+  int _selectedIndex = 1;
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onItemTapped(int selectedIndex) {
+    _pageController.animateToPage(
+      selectedIndex,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 1);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.airplane_ticket),
+            label: 'Tiket Saya',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Akun Saya',
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: _pages,
+      ),
+    );
+  }
+}
+
+// MainPage End
 
 // HomePage Start
 class HomePage extends StatelessWidget {
@@ -22,129 +100,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 80),
-            child: Container(
-              height: 50,
-              width: double.infinity,
-              margin: const EdgeInsets.only(top: 20),
-              child: const Center(
-                child: Text(
-                  'Pantai Marina',
-                  style: TextStyle(fontSize: 40, color: Colors.black54),
-                ),
-              ),
-            ),
-          ),
-
-          // Photos
-          Material(
-            color: Colors.transparent,
-            borderRadius: BorderRadius.circular(12.0),
-            child: InkWell(
-              borderRadius: BorderRadius.circular(12.0),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const PhotosPage(),
-                  )
-                );
-              },
-              child: Container(
-                width: 380,
-                height: 350,
-                margin: const EdgeInsets.only(bottom: 4.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: <Widget>[
-                    // Full-width Image
-                    ClipRRect(
-                      borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(12.0),
-                      ),
-                      child: Image.network(
-                        'https://via.placeholder.com/350x150',
-                        width: double.infinity,
-                        height: 280,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    // Text
-                    const Text(
-                      'Mengenal Lebih Dekat',
-                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-          // Action Buttons
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Material(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(12.0),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(12.0),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ReservePage(),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: 180,
-                    height: 120,
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: const Offset(0, 3)
-                        )
-                      ],
-                    ),
-                    child: const Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Icon(Icons.airplane_ticket, size: 50, color: Colors.black54),
-                        SizedBox(height: 4),
-                        Text(
-                          'Beli Tiket',
-                          style: TextStyle(fontSize: 20, color: Colors.black54),
-                        ),
-                      ],
+              Padding(
+                padding: const EdgeInsets.only(top: 40, bottom: 60),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(top: 20),
+                  child: const Center(
+                    child: Text(
+                      'Pantai Marina',
+                      style: TextStyle(fontSize: 40, color: Colors.black54),
                     ),
                   ),
                 ),
               ),
 
+              // Photos
               Material(
                 color: Colors.transparent,
                 borderRadius: BorderRadius.circular(12.0),
@@ -152,16 +128,16 @@ class HomePage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(12.0),
                   onTap: () {
                     Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ReviewPage(),
-                      ),
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PhotosPage(),
+                        )
                     );
                   },
                   child: Container(
-                    width: 180,
-                    height: 120,
-                    padding: const EdgeInsets.all(8.0),
+                    width: 380,
+                    height: 350,
+                    margin: const EdgeInsets.only(bottom: 50),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(12.0),
@@ -171,68 +147,170 @@ class HomePage extends StatelessWidget {
                           spreadRadius: 2,
                           blurRadius: 5,
                           offset: const Offset(0, 3),
-                        )
+                        ),
                       ],
                     ),
-                    child: const Column(
-                      mainAxisSize: MainAxisSize.min,
+                    child: Column(
                       children: <Widget>[
-                        Icon(Icons.chat, size: 50, color: Colors.black54),
-                        SizedBox(height: 4),
-                        Text(
-                          'Apa Kata Orang?',
-                          style: TextStyle(fontSize: 20, color: Colors.black54),
-                        )
+                        // Full-width Image
+                        ClipRRect(
+                          borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(12.0),
+                          ),
+                          child: Image.network(
+                            'https://via.placeholder.com/350x150',
+                            width: double.infinity,
+                            height: 280,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        // Text
+                        const Text(
+                          'Mengenal Lebih Dekat',
+                          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
                       ],
                     ),
                   ),
                 ),
               ),
+
+              // Action Buttons
+              Padding(
+                padding: const EdgeInsets.only(bottom: 80.0),
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Material(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(12.0),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const ReservePage(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 180,
+                          height: 120,
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3)
+                              )
+                            ],
+                          ),
+                          child: const Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Icon(Icons.airplane_ticket, size: 50, color: Colors.black54),
+                              SizedBox(height: 4),
+                              Text(
+                                'Beli Tiket',
+                                style: TextStyle(fontSize: 20, color: Colors.black54),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Material(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(12.0),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const ReviewPage(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 180,
+                          height: 120,
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: const Offset(0, 3),
+                              )
+                            ],
+                          ),
+                          child: const Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Icon(Icons.chat, size: 50, color: Colors.black54),
+                              SizedBox(height: 4),
+                              Text(
+                                'Apa Kata Orang?',
+                                style: TextStyle(fontSize: 20, color: Colors.black54),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-
-          // Footer
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            decoration: const BoxDecoration(
-              border: Border(top: BorderSide(color: Colors.grey)),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.airplane_ticket, size: 30, color: Colors.black54),
-                  onPressed: () {
-                    const Text('PlaceHolder');
-                  },
-                  tooltip: 'Tiket Saya',
-                ),
-
-                IconButton(
-                  icon: const Icon(Icons.home, size: 30, color: Colors.black54),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => const HomePage()
-                    ));
-                  },
-                ),
-                
-                IconButton(
-                  icon: const Icon(Icons.person, size: 30, color: Colors.black54),
-                  onPressed: () {
-                    const Text('PlaceHolder');
-                  },
-                  tooltip: 'Akun Saya',
-                )
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
 }
 // HomePage End
+
+// MyTicketPage Start
+class MyTicketPage extends StatelessWidget {
+  const MyTicketPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tiket Saya'),
+      ),
+    );
+  }
+}
+// MyTicketPage End
+
+// AccountPage Start
+class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Akun Saya'),
+      ),
+    );
+  }
+}
+// AccountPage End
 
 // PhotosPage Start
 class PhotosPage extends StatelessWidget {
